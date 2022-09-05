0c02dba5f2a769d3e7ab2fbb36699e0fca9f58bb
Vipul <vipulraj011@gmail.com>
2022-09-06 02:21:25 +0530

    first commit

---
+++ Whats App.py
@@ -0,0 +1,16 @@
+# Send a Hello Noise Team
+# Import pywhatkit.
+import pywhatkit
+
+# Send a Hello Noise Team
+try:
+    pywhatkit.sendwhatmsg("+919458564885","Hello Noise Team",1,00)
+    print("Sucessfully Sent")
+# Print an error message if an error occurs
+except:
+    print("An error occurred")
+
+
+
+
+
